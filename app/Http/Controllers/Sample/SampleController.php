703f760abec0f4ae6bc26b093c9379e55e98030c
T.Yajima <tatsurou.yajima@gmail.com>
2024-08-16 20:34:38 +0900

    💫 ルーティングのグループを作成

---
+++ app/Http/Controllers/Sample/SampleController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Sample;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SampleController extends Controller
+{
+    public function index(Request $request)
+    {
+        $data = [
+            'msg' => 'Sample Controller Index!',
+        ];
+        return view('hello.index', $data);
+    }
+
+    public function other(Request $request)
+    {
+        $data = [
+            'msg' => 'Sample Controller Other!',
+        ];
+        return view('hello.index', $data);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\Sample\SampleController;
 use App\Http\Middleware\HelloMiddleware;
 use Illuminate\Support\Facades\Route;
 
@@ -8,11 +9,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::middleware([HelloMiddleware::class])->group(function () {
     Route::get('/hello', [HelloController::class, 'index'])->name('hello');
     Route::get('/hello/other', [HelloController::class, 'other']);
 
     // id が数字の場合のみ index を表示する。それ以外は 404エラー にする。
     Route::get('/hello/{id}', [HelloController::class, 'index'])->where('id', '[0-9]+');
+});
+
+// Sample ディレクトリ以下をまとめて指定できるのが便利
+// http://127.0.0.1:8000/sample で SampleController の index が表示される
+Route::namespace('Sample')->group(function () {
+    Route::get('/sample', [SampleController::class, 'index']);
+    Route::get('/sample/other', 'SampleController@other');
 });
