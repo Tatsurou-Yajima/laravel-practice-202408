6144c4e6c1b2b163dcfbc7747d625be706a59f90
T.Yajima <tatsurou.yajima@gmail.com>
2024-08-16 17:00:07 +0900

    💫 Helloコントローラーを実装

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = [
+            'msg' => 'This is sample massage.',
+        ];
+        return view('hello.index', $data);
+    }
 }
