8148d5b69c85a89e18ce87c4749342e629d37185
T.Yajima <tatsurou.yajima@gmail.com>
2024-08-16 17:26:54 +0900

    💫 ミドルウェアを追加

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,17 +6,22 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    public function index(?string $id = null)
+    public function index(Request $request)
     {
         $data = [
-            'msg' => 'id = ' . $id,
+            'msg' => $request->hello,
         ];
         return view('hello.index', $data);
     }
 
-    public function other()
+    public function other(Request $request)
     {
+        $data = [
+            'msg' => $request->bye,
+        ];
         // hello というルート名を使ってリダイレクトしている
-        return redirect()->route('hello');
+        // return redirect()->route('hello');
+
+        return view('hello.index', $data);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,18 @@
 <?php
 
 use App\Http\Controllers\HelloController;
+use App\Http\Middleware\HelloMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', [HelloController::class, 'index'])->name('hello');
-Route::get('/hello/other', [HelloController::class, 'other']);
 
-// id が数字の場合のみ index を表示する。それ以外は 404エラー にする。
-Route::get('/hello/{id}', [HelloController::class, 'index'])->where('id', '[0-9]+');
+Route::middleware([HelloMiddleware::class])->group(function () {
+    Route::get('/hello', [HelloController::class, 'index'])->name('hello');
+    Route::get('/hello/other', [HelloController::class, 'other']);
+
+    // id が数字の場合のみ index を表示する。それ以外は 404エラー にする。
+    Route::get('/hello/{id}', [HelloController::class, 'index'])->where('id', '[0-9]+');
+});
