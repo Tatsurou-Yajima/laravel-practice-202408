8cc989a491ee0c5e248c2905dd0c1d2b0767a123
T.Yajima <tatsurou.yajima@gmail.com>
2024-08-16 20:43:05 +0900

    💫 config/sample を追加

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,10 +6,19 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
+    public function __construct()
+    {
+        // config/sample の message を上書きする
+        config(['sample.message' => '新しいメッセージ!']);
+    }
+
     public function index(Request $request)
     {
+        $sampleMsg = config('sample.message');
+        $sampleData = config('sample.data');
         $data = [
-            'msg' => $request->hello,
+            'msg' => $sampleMsg,
+            'data' => $sampleData,
         ];
         return view('hello.index', $data);
     }
